694688885fcaf75429e4563c93a88d2f0dfd653f
GuillaumePressiat <guillaume.pressiat@aphp.fr>
2019-03-08 14:59:00 +0100

    second time init

--- _render_book.R
+++ _render_book.R
@@ -2,3 +2,8 @@
 bookdown::render_book('index.Rmd', output_format = "bookdown::gitbook")
 bookdown::render_book('index.rmd', output_format = 'bookdown::pdf_book')
 bookdown::render_book('index.rmd', output_format = 'bookdown::epub_book')
+
+
+unlink('docs', recursive = TRUE)
+dir.create('docs')
+file.copy(list.files('_book', full.names = TRUE), 'docs', recursive = TRUE)
